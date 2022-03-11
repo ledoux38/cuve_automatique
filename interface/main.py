9e698c0c55d4dda9bec8138a932dab73ab55c683
Ledoux <florian.ledoux@synergee.com>
2022-03-11 11:03:04 +0100

    wip

--- interface/main.py
+++ interface/main.py
@@ -1,18 +1,20 @@
-from datetime import time
+import time
 
-
-from pyfirmata import Arduino
-
-from interface.packages.GPIO import GPIO
+import pyfirmata
 
 PORT: str = '/dev/ttyACM0'
 BAUDRATE: int = 921600
 TIMEOUT: int = 1
 
 if __name__ == '__main__':
-    gpio: GPIO = GPIO('d:13:o', 13)
-    carte = Arduino(PORT)
-    while True:
-        carte.digital[13].write(0)
-        time.sleep(2.4)
+    HIGH = True  # Crée un état haut qui correspond à la led allumée
+    LOW = False  # Crée un état bas qui correspond à la led éteinte
+    board = pyfirmata.Arduino(PORT)  # Initialise la communication avec la carte
+    LED_pin = board.get_pin('d:13:o')  # Initialise la broche (d => digital, 13 => N° broche, o => output)
+
+    for i in range(10):
+        LED_pin.write(HIGH)  # Allume la led
+        time.sleep(1)  # Pause de 0.5 seconde
+        LED_pin.write(LOW)  # Eteint la led
+        time.sleep(1)  #
         print("toto")
