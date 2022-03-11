f4baa6128f34dcd385188136c489bc5606630cd5
Ledoux <florian.ledoux@synergee.com>
2022-03-11 14:12:49 +0100

    wip

--- interface/main.py
+++ interface/main.py
@@ -2,19 +2,24 @@ import time
 
 import pyfirmata
 
+from interface.packages.Equipment import Element
+from interface.packages.GPIO import GPIO
+
 PORT: str = '/dev/ttyACM0'
 BAUDRATE: int = 921600
 TIMEOUT: int = 1
 
 if __name__ == '__main__':
-    HIGH = True  # Crée un état haut qui correspond à la led allumée
-    LOW = False  # Crée un état bas qui correspond à la led éteinte
-    board = pyfirmata.Arduino(PORT)  # Initialise la communication avec la carte
-    LED_pin = board.get_pin('d:13:o')  # Initialise la broche (d => digital, 13 => N° broche, o => output)
+    i: Element = Element("resistance_cuve", GPIO('d:13:o', 13))
 
-    for i in range(10):
-        LED_pin.write(HIGH)  # Allume la led
-        time.sleep(1)  # Pause de 0.5 seconde
-        LED_pin.write(LOW)  # Eteint la led
-        time.sleep(1)  #
-        print("toto")
+    # HIGH = True  # Crée un état haut qui correspond à la led allumée
+    # LOW = False  # Crée un état bas qui correspond à la led éteinte
+    # board = pyfirmata.Arduino(PORT)  # Initialise la communication avec la carte
+    # LED_pin = board.get_pin('d:13:o')  # Initialise la broche (d => digital, 13 => N° broche, o => output)
+    #
+    # for i in range(10):
+    #     LED_pin.write(HIGH)  # Allume la led
+    #     time.sleep(1)  # Pause de 0.5 seconde
+    #     LED_pin.write(LOW)  # Eteint la led
+    #     time.sleep(1)  #
+    #     print("toto")
--- interface/packages/GPIO.py
+++ interface/packages/GPIO.py
@@ -11,3 +11,12 @@ class GPIO:
 
     def get_adress(self):
         return self.adress
+
+    def set_name(self, new_name: str):
+        self.name = new_name
+
+    def set_adress(self, new_address: int):
+        self.adress = new_address
+
+    name: property = property(get_name, set_name)
+    gpio: property = property(get_adress, get_adress)
