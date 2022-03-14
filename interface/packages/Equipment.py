d285358e280950af37c12437c848be8ed8581d12
Ledoux <florian.ledoux@synergee.com>
2022-03-14 11:49:06 +0100

    wip

--- interface/packages/Equipment.py
+++ interface/packages/Equipment.py
@@ -3,20 +3,11 @@ from interface.packages.GPIO import GPIO
 
 class Element:
     def __init__(self, name: str, gpio: GPIO):
-        self.set_name(name)
-        self.set_gpio(gpio)
+        self.name = name
+        self.gpio = gpio
 
     def get_name(self):
         return self.name
 
     def get_gpio(self):
         return self.gpio
-
-    def set_name(self, name: str):
-        self.name = name
-
-    def set_gpio(self, new_gpio: GPIO):
-        self.gpio = new_gpio
-
-    name: property = property(get_name, set_name)
-    gpio: property = property(get_gpio, set_gpio)
