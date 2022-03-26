66d71cebb6b7ac7cbc071addc3ea584e4bb4e861
Ledoux38 <ledoux.florian30@gmail.com>
2022-03-26 21:51:26 +0100

    wip

--- interface/main.py
+++ interface/main.py
@@ -12,29 +12,11 @@ if __name__ == '__main__':
     window = Tk()
     window['bg'] = 'white'
 
-    # frame 1
-    Frame1 = Frame(window, relief=GROOVE)
-    Frame1.pack(side=LEFT)
-
-    # frame 2
-    Frame2 = Frame(window, relief=GROOVE)
-    Frame2.pack(side=LEFT)
-
-    # frame 3 dans frame 2
-    Frame3 = Frame(Frame2, bg="white", relief=GROOVE)
-    Frame3.pack(side=RIGHT)
-
-    # Ajout de labels
-
-    canvas = Canvas(Frame1, width=150, height=120, background='yellow')
-    canvas.pack(padx=10, pady=10)
-    ligne1 = canvas.create_line(75, 0, 75, 120)
-    ligne2 = canvas.create_line(0, 60, 150, 60)
-    txt = canvas.create_text(75, 60, text="Cible", font="Arial 16 italic", fill="blue")
-
-
-    Label(Frame2, text="Frame 2").pack(padx=10, pady=10)
-    Label(Frame3, text="Frame 3", bg="white").pack(padx=10, pady=10)
+    Button(window, text='L1-C1', borderwidth=1).grid(row=1, column=1)
+    Button(window, text='L1-C2', borderwidth=1).grid(row=1, column=2)
+    Button(window, text='L2-C3', borderwidth=1).grid(row=2, column=3)
+    Button(window, text='L2-C4', borderwidth=1).grid(row=2, column=4)
+    Button(window, text='L3-C3', borderwidth=1).grid(row=3, column=3)
     window.mainloop()
 
     # element: Element = Element("carte", [IO('d:13:o', 'led')], PORT)
