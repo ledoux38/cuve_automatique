92e043abf08255d957190bf30a868c50abafa604
Ledoux38 <ledoux.florian30@gmail.com>
2022-04-04 22:14:39 +0200

    wip

--- interface/main.py
+++ interface/main.py
@@ -12,11 +12,25 @@ if __name__ == '__main__':
     window = Tk()
     window['bg'] = 'white'
 
-    Button(window, text='L1-C1', borderwidth=1).grid(row=1, column=1)
-    Button(window, text='L1-C2', borderwidth=1).grid(row=1, column=2)
-    Button(window, text='L2-C3', borderwidth=1).grid(row=2, column=3)
-    Button(window, text='L2-C4', borderwidth=1).grid(row=2, column=4)
-    Button(window, text='L3-C3', borderwidth=1).grid(row=3, column=3)
+    canvasFrame = Frame(window)
+    canvasFrame.pack(side=LEFT)
+
+    buttonFrame = Frame(window)
+    buttonFrame.pack(side=RIGHT)
+
+    # canvas
+    canvas = Canvas(canvasFrame, width=150, height=120, background='yellow')
+    ligne1 = canvas.create_line(75, 0, 75, 120)
+    ligne2 = canvas.create_line(0, 60, 150, 60)
+    txt = canvas.create_text(75, 60, text="Cible", font="Arial 16 italic", fill="blue")
+    canvas.pack()
+
+
+    Button(buttonFrame, text='L1-C1', borderwidth=1).grid(row=1, column=1)
+    Button(buttonFrame, text='L1-C2', borderwidth=1).grid(row=1, column=2)
+    Button(buttonFrame, text='L2-C3', borderwidth=1).grid(row=2, column=3)
+    Button(buttonFrame, text='L2-C4', borderwidth=1).grid(row=2, column=4)
+    Button(buttonFrame, text='L3-C3', borderwidth=1).grid(row=3, column=3)
     window.mainloop()
 
     # element: Element = Element("carte", [IO('d:13:o', 'led')], PORT)
